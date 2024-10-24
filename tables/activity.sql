f2df19f9c9e1af4f4ac3b85153bcc8194f388598
matt pero <mattpero22@gmail.com>
2024-10-24 09:38:18 -0400

    added activity table to store activity types. Will be tied to individuals via a person_to_activity table

---
+++ tables/activity.sql
@@ -0,0 +1,24 @@
+-- Create the table
+CREATE TABLE activity (
+    activity_id SMALLINT UNSIGNED AUTO_INCREMENT,
+    activity_name VARCHAR(40),
+    activity_description VARCHAR(255),
+    activity_category ENUM('Chores', 'Study', 'Health', 'Cook', 'Relax'),
+    date_created DATE,
+    date_updated DATE,
+    is_active BOOLEAN,
+    time_update DATETIME,
+    CONSTRAINT pk_activity PRIMARY KEY (activity_id)
+);
+
+-- Set default value for the date_created column
+ALTER TABLE activity MODIFY date_created DATE DEFAULT (CURDATE());
+
+-- Set default value for the date_updated column
+ALTER TABLE activity MODIFY date_updated DATE DEFAULT (CURDATE());
+
+-- Set default value for the is_active column
+ALTER TABLE activity MODIFY is_active BOOLEAN DEFAULT 0;
+
+-- Set default value for the date_updated column
+ALTER TABLE activity MODIFY time_update DATETIME DEFAULT (CURRENT_TIMESTAMP());
