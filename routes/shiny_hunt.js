1071c6ad574f5834b5e688ca1f586d60ede9967c
matt pero <mattpero22@gmail.com>
2024-11-20 17:09:49 -0500

    updated naming convention for the locations column to be UPPER

--- routes/shiny_hunt.js
+++ routes/shiny_hunt.js
@@ -21,8 +21,9 @@ module.exports = (db) => {
   });
 
   // GET - one hunt by shiny_hunt_id - ./api/shiny-hunt/:shiny_hunt_id
-  router.get('/', (req, res) => {
-    const query = `SELECT * FROM shiny_hunt WHERE shiny_hunt_id = ${req.body.shiny_hunt_id}`
+  router.get('/:shiny_hunt_id', (req, res) => {
+    const {shiny_hunt_id } = req.params
+    const query = `SELECT * FROM shiny_hunt WHERE shiny_hunt_id = ${shiny_hunt_id}`
     db.query(query, (err, results) => {
       if (err) {
         res.status(500).json({ error: err.message })
@@ -54,7 +55,32 @@ module.exports = (db) => {
             is_active, 
             time_update 
         ) VALUES (
-            '${req.body.ot}','${req.body.game}','${req.body.shiny_hunt_date}',${req.body.num_encounters},${req.body.num_shiny},'${req.body.locations}','${req.body.target_pokemon}',${req.body.target_encounters},CURDATE(),CURDATE(),true,NOW())`
+            '${req.body.ot}',
+            '${req.body.game}',
+            '${req.body.shiny_hunt_date}',
+            ${req.body.num_encounters},
+            ${req.body.num_shiny},
+            'UPPER(${req.body.locations})',
+            '${req.body.target_pokemon}',
+            ${req.body.target_encounters},
+            CURDATE(),
+            CURDATE(),
+            true,
+            NOW())`
+    db.query(query, (err, results) => {
+      if (err) {
+        res.status(500).json({ error: err.message })
+      } else {
+        res.json(results);
+      }
+    });
+  })
+
+
+  // DELETE - delete a hunt by shiny_hunt_id - ./api/shiny-hunt/delete/:shiny_hunt_id
+  router.delete('/delete/:shiny_hunt_id', (req, res) => {
+    const {shiny_hunt_id } = req.params
+    const query = `DELETE FROM shiny_hunt WHERE shiny_hunt_id = ${shiny_hunt_id}`
     db.query(query, (err, results) => {
       if (err) {
         res.status(500).json({ error: err.message })
